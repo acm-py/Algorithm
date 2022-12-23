9e646ccefd455ef1bbb162bfbc4852009c6443bb
km_qmt_pro <km_qmt_pro@qq.com>
2022-12-23 22:04:26 +0800

    upload basic setting

---
+++ leetcoder/705.py
@@ -0,0 +1,11 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+@Author         :  Brisky
+@Version        :  WIN10, Python3.7.9
+------------------------------------
+@IDE            ： PyCharm-> 705.py
+@Description    :  
+@CreateTime     :  2022/12/23 22:03
+------------------------------------
+"""
