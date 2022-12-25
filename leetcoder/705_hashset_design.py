9045b6e6f5b3b858ff1aee09cb33ce3beb6e9d6b
km_qmt_pro <km_qmt_pro@qq.com>
2022-12-25 12:09:13 +0800

    upload basic setting

---
+++ leetcoder/705_hashset_design.py
@@ -0,0 +1,13 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+@Author         :  Brisky
+@Version        :  WIN10, Python3.7.9
+------------------------------------
+@IDE            ： PyCharm-> 705_hashset_design.py
+@Description    :
+参考
+https://leetcode.cn/problems/design-hashset/solutions/653252/xiang-jie-hashset-de-she-ji-zai-shi-jian-4plc/?languageTags=python3
+@CreateTime     :  2022/12/23 22:03
+------------------------------------
+"""
